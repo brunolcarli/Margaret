eef190da7fb28ed54a252939442ad4188fffd2dc
brunolcarli <brunolcarli@gmail.com>
2023-07-29 03:54:14 -0300

    ADD user model

---
+++ src/models.py
@@ -0,0 +1,10 @@
+
+
+class User:
+    def __init__(self, id, member_id, name, challenges, score, last_update):
+        self.id = id
+        self.member_id = member_id
+        self.name = name
+        self.challenges = challenges
+        self.score = score
+        self.last_update = last_update
